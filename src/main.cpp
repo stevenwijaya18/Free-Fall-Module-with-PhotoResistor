e1b03e43ec9f49a0dffa876b965c25b86258a01d
Steven <wijayasteven070@gmail.com>
2026-01-07 17:12:19 +0700

    Added Proteus

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <Arduino.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
 
-#define MAGNET_PIN 6
-#define SENSOR_PIN 3
+#define MAGNET_PIN_BIT PD6 
+#define SENSOR_PIN_BIT PD3
 
 const uint16_t TIMER_RELOAD = 65285;
 const uint8_t SPACING = 10;
@@ -12,80 +14,83 @@ volatile uint8_t hitung_sensor = 0;
 volatile bool flag_start = false;
 volatile bool request_print = false;
 
-void isr_sensor();
+ISR(TIMER1_OVF_vect) {
+    TCNT1 = TIMER_RELOAD;
+    timer_1ms++;
+}
+
+ISR(INT1_vect) {
+    if (flag_start) {
+        hitung_sensor++;
+        
+        if (hitung_sensor <= 10) {
+            jarak += SPACING;
+            request_print = true;
+            
+            if (hitung_sensor == 10) {
+                TCCR1B &= ~((1 << CS12) | (1 << CS11) | (1 << CS10));
+                flag_start = false;
+            }
+        }
+    }
+}
 
 void setup() {
-  Serial.begin(115200);
-  
-  pinMode(MAGNET_PIN, OUTPUT);
-  digitalWrite(MAGNET_PIN, HIGH);
-  
-  pinMode(SENSOR_PIN, INPUT);
-  attachInterrupt(digitalPinToInterrupt(SENSOR_PIN), isr_sensor, RISING);
+    // --- CHANGED BAUDRATE HERE ---
+    Serial.begin(57600);
 
-  noInterrupts();
-  TCCR1A = 0;
-  TCCR1B = 0;
-  TCNT1 = TIMER_RELOAD;
-  TIMSK1 |= (1 << TOIE1);
-  interrupts();
+    DDRD |= (1 << MAGNET_PIN_BIT);
+    PORTD |= (1 << MAGNET_PIN_BIT);
+
+    DDRD &= ~(1 << SENSOR_PIN_BIT);
+
+    cli();
+
+    EICRA |= (1 << ISC11) | (1 << ISC10);
+    EIMSK |= (1 << INT1);
+
+    TCCR1A = 0;
+    TCCR1B = 0;
+    TCNT1 = TIMER_RELOAD;
+    TIMSK1 |= (1 << TOIE1);
+
+    sei();
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    char ds = Serial.read();
-    
-    if (ds == 'S' || ds == 's') {
-      digitalWrite(MAGNET_PIN, LOW);
-      
-      TCCR1B = 0;
-      TCNT1 = TIMER_RELOAD;
-      
-      timer_1ms = 0;
-      jarak = 0;
-      hitung_sensor = 0;
-      flag_start = true;
-      
-      Serial.print(jarak);
-      Serial.print(":");
-      Serial.print(timer_1ms);
-      Serial.println("#");
-      
-      TCCR1B |= (1 << CS11) | (1 << CS10);
+    if (Serial.available() > 0) {
+        char ds = Serial.read();
+
+        if (ds == 'S' || ds == 's') {
+            PORTD &= ~(1 << MAGNET_PIN_BIT);
+
+            TCCR1B = 0;
+            TCNT1 = TIMER_RELOAD;
+            
+            timer_1ms = 0;
+            jarak = 0;
+            hitung_sensor = 0;
+            flag_start = true;
+
+            Serial.print(jarak);
+            Serial.print(":");
+            Serial.print(timer_1ms);
+            Serial.println("#");
+
+            TCCR1B |= (1 << CS11) | (1 << CS10);
+        }
     }
-  }
 
-  if (request_print) {
-    noInterrupts();
-    uint16_t current_jarak = jarak;
-    uint32_t current_timer = timer_1ms;
-    request_print = false;
-    interrupts();
+    if (request_print) {
+        cli();
+        uint16_t cetak_jarak = jarak;
+        uint32_t cetak_timer = timer_1ms;
+        request_print = false;
+        sei();
 
-    Serial.print(current_jarak);
-    Serial.print(":");
-    Serial.print(current_timer);
-    Serial.println("#");
-  }
-}
-
-ISR(TIMER1_OVF_vect) {
-  TCNT1 = TIMER_RELOAD;
-  timer_1ms++;
-}
-
-void isr_sensor() {
-  if (flag_start) {
-    hitung_sensor++;
-    
-    if (hitung_sensor <= 10) {
-      jarak += SPACING;
-      request_print = true;
-      
-      if (hitung_sensor == 10) {
-        TCCR1B &= ~((1 << CS12) | (1 << CS11) | (1 << CS10));
-        flag_start = false;
-      }
+        Serial.print(cetak_jarak);
+        Serial.print(":");
+        Serial.print(cetak_timer);
+        Serial.println("#");
     }
-  }
 }
